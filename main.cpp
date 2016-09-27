0e1cad97c73c8c861de2ab738f08d3cfc9268d77
Julian Andres Guarin <julian.guarin@fitech.com.co>
2016-09-27 07:32:35 -0500

    Register Uld filter project

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include "uldfilterproject.h"
 
 
 
-
-
+void registerExternalType(){
+    qmlRegisterType<Uldfilterproject>("com.autoformax.videoprocess",1,0,"VideoCapture");    
+}
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     
     QQmlApplicationEngine engine;
+    registerExternalType();
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     
     return app.exec();
